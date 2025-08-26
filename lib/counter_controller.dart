0f5698e5b7b6eff8c2787c8dfdd042dc944662c0
Hassan Abdu <hassan.elegeimy@gmail.com>
2025-08-27 01:45:13 +0300

    selector

--- lib/counter_controller.dart
+++ lib/counter_controller.dart
@@ -3,8 +3,26 @@ import 'package:flutter/cupertino.dart';
 class CounterController extends ChangeNotifier {
   int counter = 0;
 
+  String? userName;
+  String? age;
+
+  void setUserName(String? userName) {
+    this.userName = userName;
+    notifyListeners();
+  }
+
+  void setAge(String? age) {
+    this.age = age;
+    notifyListeners();
+  }
+
   void incrementCounter() {
     counter++;
+    notifyListeners();
+  }
+
+  void decrementCounter() {
+    counter--;
     notifyListeners();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,20 +14,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
       home: ChangeNotifierProvider(
         create: (BuildContext context) => CounterController(),
-        child: const MyHomePage(title: 'Flutter Demo Home Page'),
+        child: MyHomePage(title: 'Flutter Demo Home Page'),
       ),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
+  MyHomePage({super.key, required this.title});
+
   final String title;
+  final TextEditingController userNameController = TextEditingController();
+  final TextEditingController ageController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -43,27 +44,81 @@ class MyHomePage extends StatelessWidget {
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
             Consumer(
-              builder: (
-                BuildContext context,
-                CounterController value,
-                Widget? child,
-              ) {
+              builder: (BuildContext context, CounterController value, Widget? child) {
                 print("change  counter text");
-                return Text(
-                  '${value.counter}',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                );
+                return Text('${value.counter}', style: Theme.of(context).textTheme.headlineMedium);
               },
+            ),
+            SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: TextFormField(controller: userNameController),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                context.read<CounterController>().setUserName(userNameController.text);
+              },
+              child: Text("save username"),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: TextFormField(controller: ageController),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                context.read<CounterController>().setAge(ageController.text);
+              },
+              child: Text("save age"),
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Selector(
+                  selector: (BuildContext context, CounterController controller) {
+                    return controller.userName ?? "None";
+                  },
+                  builder: (BuildContext context, String? userName, Widget? child) {
+                    print("change username");
+                    return Text("$userName");
+                  },
+                ),
+                Selector(
+                  selector: (BuildContext context, CounterController controller) {
+                    return controller.age ?? "None";
+                  },
+                  builder: (BuildContext context, String? age, Widget? child) {
+                    print("change age");
+                    return Text("$age");
+                  },
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.read<CounterController>().incrementCounter();
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              context.read<CounterController>().incrementCounter();
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          SizedBox(height: 20),
+          FloatingActionButton(
+            onPressed: () {
+              context.read<CounterController>().decrementCounter();
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.minimize),
+          ),
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
